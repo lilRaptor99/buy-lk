5c0f4acb365097adf79372c36410ef0d94857ddd
Dilshi Navodya <62331157+DilshiNavodya@users.noreply.github.com>
2022-09-14 04:04:48 +0530

    login signup seller ui done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,25 @@
 import logo from "./logo.svg";
+import React from 'react'
 import "./App.css";
-import KidSection from "./KidSection";
-// import { BrowserRouter, Route, Routes } from "react-router-dom";
+import KidSection from "./Pages/KidSection";
+import Login from './Pages/Login';
+import Landingpage from "./Pages/LandingPage";
+import Advertise from "./Pages/Advertise";
+import Signup from "./Pages/Signup";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  return <KidSection />;
+  return (
+    <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Landingpage/>}/>
+          <Route path='/login' element={<Login/>}/>
+          <Route path='/signup' element={<Signup/>}/>
+          <Route path='/advertise' element={<Advertise/>}/>
+          <Route path='/kids' element={<KidSection/>}/>
+        </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
