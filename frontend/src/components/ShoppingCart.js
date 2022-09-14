2339736673e1e968acb15e56a66f66d34f02efdd
Pratheek Senevirathne <pratheeksenevirathne@gmail.com>
2022-09-14 16:14:58 +0530

    Add shoppin cart

--- frontend/src/components/ShoppingCart.js
+++ frontend/src/components/ShoppingCart.js
@@ -1,18 +1,19 @@
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
 import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
 import KeyboardArrowUpIcon from "@mui/icons-material/KeyboardArrowUp";
+import CloseIcon from "@mui/icons-material/Close";
 import React, { useState } from "react";
 import "./ShoppingCart.css";
 
 export default function ShoppingCart({ cartItems }) {
-  const [isOpen, setIsOpen] = useState(true);
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
     <div style={{ position: "relative" }}>
       <div className="cart-button-wrapper">
         <button className="cart-button" onClick={() => setIsOpen(!isOpen)}>
           <ShoppingCartIcon scale={100} style={{ marginRight: "20px" }} />
-          <h4>Cart</h4>
+          <div>Cart</div>
           {!isOpen ? (
             <KeyboardArrowDownIcon
               scale={30}
@@ -33,11 +34,30 @@ export default function ShoppingCart({ cartItems }) {
         <div className="cart-list-item">
           <div className="cart-list-item-image"></div>
           <div className="cart-list-item-data">
-            <p className="">
-              <span>3 x </span> Kids T-Shirt
-            </p>
-            <p className="cart-list-item-price">$30.00</p>
+            <div className="cart-list-item-name">
+              <span className="cart-list-item-name-qty">3 x </span> Kids T-Shirt
+            </div>
+            <div className="cart-list-item-price">$30.00</div>
           </div>
+          <button className="cart-list-item-remove">
+            <CloseIcon style={{ fontSize: "15px" }} />
+          </button>
+        </div>
+        <div className="cart-list-item">
+          <div className="cart-list-item-image"></div>
+          <div className="cart-list-item-data">
+            <div className="cart-list-item-name">
+              <span className="cart-list-item-name-qty">1 x </span> Kids Toy
+            </div>
+            <div className="cart-list-item-price">$30.00</div>
+          </div>
+          <button className="cart-list-item-remove">
+            <CloseIcon style={{ fontSize: "15px" }} />
+          </button>
+        </div>
+
+        <div className="cart-list-checkout-btn-wrapper">
+          <button className="cart-list-checkout-btn">Checkout</button>
         </div>
       </div>
     </div>
